03d5f14309c2f2857b08822ffa9cd09d5cc59820
NgoVoQuy1810 <ngovoquy2006@gmail.com>
2026-09-25 22:53:16 +0700

    khoi tao tv an toan

--- app/src/main/java/com/example/studentprofile/MainActivity.kt
+++ app/src/main/java/com/example/studentprofile/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.studentprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Bước 1: Khai báo biến binding với lateinit var
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Bước 2: Nạp layout XML thông qua hàm inflate()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Bước 3: Truyền root view vào setContentView
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Bước 4: Thao tác View trực tiếp, không lo Null!
+        binding.tvWelcome.text = "Chào mừng bạn đến với ViewBinding!"
     }
 }
